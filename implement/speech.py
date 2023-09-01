400ce19e4117b0aa592eb45603573c3f56de11fc
NickSheva <nikolayshevtsov@mail.ru>
2023-09-01 12:27:55 +0500

    changed .mp4 file to .wav and saved

--- implement/speech.py
+++ implement/speech.py
@@ -4,7 +4,10 @@ from moviepy.editor import *
 
 file_path = Path("MTS.mp4")
 if file_path.exists() and file_path.is_file():
-    print("Success!")
+    video = VideoFileClip(f"{file_path}")
+    audio = video.audio
+    audio.write_audiofile("MTS.wav")
+    print("Success")
 else:
     print("File does not exist")
 
